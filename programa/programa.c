ca8d832bc9c4d2aba54b83f0f20f1eb0fedc5f65
Kevin Salazar <80077213+Kevin-Salazar-itcr@users.noreply.github.com>
2023-08-19 14:39:57 -0600

    Arreglo de detalles
    Co-Authored-By: adjany08 <84366189+adjany08@users.noreply.github.com>

--- programa/programa.c
+++ programa/programa.c
@@ -10,7 +10,6 @@
  */
 bool menu(){
     char opcion;
-    system("clear");
     printf("      o    ~~~^-< ### >-^~~~    o\n      (___/ Biblioteca V1.0 \\___)\n********** *Menu de opciones* ***********\n");
     printf("\t1. Gestion de catalogo\n\t2. Gestion de usuarios 2\n\t3. Historial de prestamos\n");
     printf("\t4. Vencimiento de prestamos\n\t5. Estadisticas\n\t6. Salir\n");
@@ -20,37 +19,22 @@ bool menu(){
     switch (opcion){
         case '1':{
 	        printf("\ncaso 1\n");
-            char c;
-            printf("Presione una tecla para continuar...\n");
-            scanf(" %c", &c);
             break;
         }
         case '2':{
 	        printf("\ncaso 2\n");
-            char c;
-            printf("Presione una tecla para continuar...\n");
-            scanf(" %c", &c);
             break;
         }
         case '3':{
 	        printf("\ncaso 3\n");
-            char c;
-            printf("Presione una tecla para continuar...\n");
-            scanf(" %c", &c);
             break;
         }
         case '4':{
 	        printf("\ncaso 4\n");
-            char c;
-            printf("Presione una tecla para continuar...\n");
-            scanf(" %c", &c);
             break;
         }
         case '5':{
 	        printf("\ncaso 5\n");
-            char c;
-            printf("Presione una tecla para continuar...\n");
-            scanf(" %c", &c);
             break;
         }
         case '6':{
@@ -61,10 +45,7 @@ bool menu(){
         default:{
             printf("\nOpcion no valida\n");
 	        //En Linux no existe el system("pause") como en windows, se usa esto
-            char c;
-            printf("Presione una tecla para continuar...\n");
-            scanf(" %c", &c);
-            system("clear");
+             system("clear");
             break;
         }
     }
