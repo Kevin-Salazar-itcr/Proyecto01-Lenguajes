39a6fced463ca17651bf00d99f4aeca84dd0c9f9
adjany08 <84366189+adjany08@users.noreply.github.com>
2023-08-26 17:13:01 -0600

    Add files via upload
    Mas opciones

--- programa/opcionesOperativas.c
+++ programa/opcionesOperativas.c
@@ -1,6 +1,77 @@
 #include <stdio.h>
 #include <string.h>
-#include <structs.h>
+
+struct Prestamo incluir_Prestamo()
+{
+    struct Prestamo Prestamo;
+    
+    printf("Ingrese el id del prestamo: ");
+    scanf("%d", &Prestamo.id);
+    printf("Ingrese el nombre del usuario: ");
+    scanf("%s", Prestamo.usuario);
+    printf("Ingrese el estado del prestamo: ");
+    scanf("%d", &Prestamo.estado);
+    printf("Ingrese el nombre del libro: ");
+    scanf("%s", Prestamo.nombre);
+    printf("Ingrese la fecha de inicio del prestamo: ");
+    scanf("%s", Prestamo.fechaInicio);
+    printf("Ingrese la fecha de fin del prestamo: ");
+    scanf("%s", Prestamo.fechaFin);
+
+    return Prestamo;
+
+};
+
+
+void mostrarPrestamosRango()
+{
+
+    char fechaInicio[11];
+    char fechaFin[11];
+
+    int indice = 0;
+
+    printf("Ingrese la fecha de inicio: ");
+    scanf("%s", fechaInicio);
+    printf("Ingrese la fecha de fin: ");
+    scanf("%s", fechaFin);
+
+    for (int i = 0; i < indice; i++)
+    {
+        if (strcmp(prestamos[i].fechaPedido, fechaInicio) >= 0 && strcmp(prestamos[i].fechaRecibido, fechaFin) <= 0)
+        {
+            printf("\nDetalles del Prestamo:\n");
+            printf("Id: %d\n", prestamos[i].id);
+            printf("Usuario: %s\n", prestamos[i].usuario);
+            printf("Estado: %d\n", prestamos[i].estado);
+            printf("Nombre: %s\n", prestamos[i].nombre);
+            printf("Fecha de inicio: %s\n", prestamos[i].fechaInicio);
+            printf("Fecha de fin: %s\n", prestamos[i].fechaFin);
+
+        }
+    };
+
+};
+
+
+struct Prestamo incluir_Prestamo()
+{
+    struct Prestamo Prestamo;
+    
+    printf("Ingrese la identificacion del usuario: ");
+    scanf("%d", &usuario.id);
+
+    printf("Ingrese el nombre del usuario: ");
+    scanf(" %s", usuario.nombre);
+    printf("Ingrese la direccion del usuario: ");
+    scanf(" %s", usuario.direccion);
+    
+    return usuario;
+};
+
+
+
+
 
 struct Usuario incluir_Usuario()
 {
