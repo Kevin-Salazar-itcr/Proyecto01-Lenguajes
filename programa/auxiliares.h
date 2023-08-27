1db81dbedea4df3a3c0a430535a4d81ca86583e5
Kevin Salazar <80077213+Kevin-Salazar-itcr@users.noreply.github.com>
2023-08-27 01:36:13 -0600

    Opciones generales
    Commit olvidado (25/08)
    Se creo la base para la utilizacion de las funciones de busqueda particular, avanzada y funciones de solicitud y devolucion de prestamos

---
+++ programa/auxiliares.h
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+
+/**
+ * @brief funcion que busca coincidencias de un texto en un archivo
+ * @param textoBuscar texto a buscar en el archivo
+ * @param texto texto donde se quiere buscar una coincidencia
+ * @return 1 si se encontraron coincidencias, 0 si no se encontraron
+ */
+int buscarCoincidencias(char* textoBuscar, char* texto);
+
+/**
+ * @brief funcion que solicita un texto al usuario para buscar en los archivos
+ * muestra todos los id nombre y resumen de los archivos que contengan el texto solicitado
+ */ 
+void busquedaSimple(){
+    char* texto = calloc(100, sizeof(char));
+    printf("Ingrese el texto a buscar: ");
+    scanf(" %[^\n]s", texto);
+    //aca se obtienen todos los nombres de los archivos
+    
+    //pseudocodigo del algoritmo
+    //contador para mostrar el numero de coincidencias
+    int n = 0;
+    //por cada nombre de archivo
+        //se llama a la funcion buscarCoincidencias
+        //si la funcion retorna 1
+            //se muestra el id, nombre y resumen del archivo
+    printf("Se encontraron %d coincidencias\n", n);
+    free(texto);
+}
+
+/**
+ * @brief funcion auxiliar para obtener la fecha actual
+ * @return fecha actual en formato dd/mm/aaaa
+ */
+char* fechaActual(){
+    time_t currentTime;
+    struct tm *localTime;
+    time(&currentTime);         // Obtiene el tiempo actual en segundos desde 1970
+    localTime = localtime(&currentTime); // Convierte el tiempo en una estructura tm
+    
+    char* fecha = calloc(15, sizeof(char));
+    sprintf(fecha, "%02d/%02d/%04d", localTime->tm_mday, localTime->tm_mon + 1, localTime->tm_year + 1900);
+    return fecha;
+}
+
+/**
+ * @brief funcion auxiliar para saber si una fecha es valida
+ * 
+ * @param cadena la cadena que contiene la fecha
+ * @return true si la fecha es valida
+ * @return false si la fecha no es valida
+ */
+bool esFechaValida(char *cadena) {
+    int dia, mes, anio;
+    if (sscanf(cadena, "%d/%d/%d", &dia, &mes, &anio) != 3) {
+        return false; // No se pudieron obtener tres valores enteros
+    }
+
+    if (mes < 1 || mes > 12 || dia < 1) {
+        return false; // Mes y día fuera de rangos válidos
+    }
+
+    switch (mes) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            return dia <= 31; // Meses con 31 días
+        case 4: case 6: case 9: case 11:
+            return dia <= 30; // Meses con 30 días
+        case 2:
+            if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0)) {
+                return dia <= 29; // Año bisiesto, febrero tiene 29 días
+            } else {
+                return dia <= 28; // Febrero tiene 28 días
+            }
+        default:
+            return false; // Mes no válido
+    }
+}
+
+/**
+ * @brief funcion auxiliar para comparar dos fechas
+ * 
+ * @param fecha1 la primera fecha
+ * @param fecha2 la segunda fecha
+ * @return int 0 si las fechas son iguales, 1 si la primera fecha es mayor, -1 si la segunda fecha es mayor
+ */
+int compararFechas(char* fecha1, char* fecha2) {
+    int dia1, mes1, ano1;
+    int dia2, mes2, ano2;
+
+    // Dividir la cadena de fecha1 en día, mes y año
+    sscanf(fecha1, "%d/%d/%d", &dia1, &mes1, &ano1);
+
+    // Dividir la cadena de fecha2 en día, mes y año
+    sscanf(fecha2, "%d/%d/%d", &dia2, &mes2, &ano2);
+
+    // Comparar años
+    if (ano1 > ano2) {
+        return 1;
+    } else if (ano1 < ano2) {
+        return -1;
+    }
+
+    // Comparar meses si los años son iguales
+    if (mes1 > mes2) {
+        return 1;
+    } else if (mes1 < mes2) {
+        return -1;
+    }
+
+    // Comparar días si los años y meses son iguales
+    if (dia1 > dia2) {
+        return 1;
+    } else if (dia1 < dia2) {
+        return -1;
+    }
+
+    // Las fechas son iguales
+    return 0;
+}
+
---
+++ programa/opcionesGenerales.h
@@ -0,0 +1,117 @@
+#include "auxiliares.h"
+
+/**
+ * @brief funcion que solicita un texto al usuario para buscar en los archivos
+ * muestra todos los id nombre y resumen de los archivos que contengan el texto solicitado
+ */ 
+void busquedaSimple(){
+    char* texto = calloc(100, sizeof(char));
+    printf("Ingrese el texto a buscar: ");
+    scanf(" %[^\n]s", texto);
+    //aca se obtienen todos los nombres de los archivos
+    
+    //pseudocodigo del algoritmo
+    //contador para mostrar el numero de coincidencias
+    int n = 0;
+    //por cada nombre de archivo
+        //se llama a la funcion buscarCoincidencias
+        //si la funcion retorna 1
+            //se muestra el id, nombre y resumen del archivo
+    printf("Se encontraron %d coincidencias\n", n);
+    free(texto);
+}
+
+/**
+ * @brief funcion que solicita datos especificos al usuario para buscar en los archivos
+ * muestra todos los id nombre y resumen de los archivos que contengan los datos solicitados por aparte o juntos a la vez
+ */ 
+void busquedaAvanzada(){
+    char** datos = calloc(6, sizeof(char*));
+    for (int i = 0; i < 6; i++){
+        datos[i] = calloc(100, sizeof(char));
+
+    }
+    printf("Ingrese el nombre a buscar (0 si no desea buscar por este rubro): ");
+    scanf(" %[^\n]s", &datos[0]);
+    printf("Ingrese el autor a buscar (0 si no desea buscar por este rubro): ");
+    scanf(" %[^\n]s", &datos[1]);
+    printf("Ingrese el genero a buscar (0 si no desea buscar por este rubro): ");
+    scanf(" %[^\n]s", &datos[2]);
+    printf("Ingrese el resumen a buscar (0 si no desea buscar por este rubro): ");
+    scanf(" %[^\n]s", &datos[3]);
+    
+    while(true){
+        printf("Desea que se busque por coincidencia exacta? (s/n): ");
+        scanf(" %[^\n]s", &datos[4]);
+        if (strcmp(datos[4], "s") == 0 || strcmp(datos[4], "n") == 0){
+            break;
+        }else{
+            printf("Ingrese una opcion valida\n");
+        }
+    }
+
+    while(true){
+        printf("Desea que se busque por todos los rubros? (s/n): ");
+        scanf(" %[^\n]s", &datos[5]);
+        if (strcmp(datos[5], "s") == 0 || strcmp(datos[5], "n") == 0){
+            break;
+        }else{
+            printf("Ingrese una opcion valida\n");
+        }
+    }
+    printf("\n\n*********Iniciando busqueda*********\n\n");
+    //aca se obtienen todos los nombres de los archivos
+    
+    //pseudocodigo del algoritmo
+    //contador para mostrar el numero de coincidencias
+    int n = 0;
+    //si el campo 6 dice que se quiere por todos los rubros
+        //por cada nombre de archivo
+            //llamar funcion aux buscarTodasCoinc(..., datos[5]) para saber si se busca por palabra exacta o coincidencia
+            //si la funcion retorna 1
+                //se muestra el id, nombre y resumen del archivo
+    //si el campo 6 dice que no se quiere por todos los rubros
+        //por cada nombre de archivo
+            //llamar funcion aux buscarCoinc(..., datos[5]) para saber si se busca por palabra exacta o coincidencia
+            //si la funcion retorna 1
+                //se muestra el id, nombre y resumen del archivo
+    printf("Se encontraron %d coincidencias\n", n);
+    free(texto);
+}
+
+/**
+ * @brief funcion para hacer prestamo de un libro
+ * 
+ */
+void prestamoEjemplar(){
+    printf("indique su usuario: ");
+    char* usuario = calloc(100, sizeof(char));
+    scanf(" %[^\n]s", usuario);
+    printf("indique el id del ejemplar: ");
+    char* id = calloc(100, sizeof(char));
+    scanf(" %[^\n]s", id);
+    printf("\nEstimado usuario, la fecha de su prestamo sera situada desde hoy y hasta la fecha que indique\n");
+
+    char* fechaPrestamo = fechaActual();
+    char* fechaDevolucion = calloc(15, sizeof(char));
+    while(true){
+        printf("indique la fecha de devolucion en formato dd/mm/aaaa: ");
+        scanf(" %[^\n]s", fechaDevolucion);
+        if (esFechaValida(fechaDevolucion)){
+            break;
+        }else{
+            printf("Ingrese una fecha valida\n");
+        }
+
+        //se valida que la fecha de devolucion sea mayor a la fecha actual
+        int valido = compararFechas(fechaPrestamo, fechaDevolucion);
+        if (valido == 1 || valido == 0){
+            printf("La fecha de devolucion debe ser mayor a la fecha actual\n\n");
+        }else{
+            break;
+        }
+    }
+
+    //validar disponibilidad del ejemplar
+}
+
