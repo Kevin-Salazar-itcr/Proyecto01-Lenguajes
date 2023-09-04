b5c5cd0d52e90ba30c1733050a731418c7f41290
Kevin Salazar <80077213+Kevin-Salazar-itcr@users.noreply.github.com>
2023-09-03 20:25:07 -0600

    Update opcionesOperativas.h

--- programa/opcionesOperativas.h
+++ programa/opcionesOperativas.h
@@ -10,13 +10,13 @@ void mostrarPrestamosRango(listaPrestamos* l)
 
     while(true){
         printf("Ingrese la fecha de inicio: ");
-        scanf(" %[^\n]s", &fechaInicio);
+        scanf(" %[^\n]s", fechaInicio);
         
         printf("Ingrese la fecha de fin: ");
-        scanf(" %[^\n]s", &fechaFin);
+        scanf(" %[^\n]s", fechaFin);
         
         if (esFechaValida(fechaInicio) && esFechaValida(fechaFin)){
-            if(compararFechas(prestamo.fechaInicio, prestamo.fechaFin) == 1){
+            if(compararFechas(prestamo->fechaInicio, prestamo->fechaFin) == 1){
                 break;
             }
             else{
@@ -31,15 +31,15 @@ void mostrarPrestamosRango(listaPrestamos* l)
     Prestamo* aux = l->inicio;
     while (aux != NULL)
     {
-        if (compararFechas(aux.fechaInicio, fechaInicio) >= 0 && compararFechas(aux.fechaFin, fechaFin) <= 0)
+        if (compararFechas(aux->fechaInicio, fechaInicio) >= 0 && compararFechas(aux->fechaFin, fechaFin) <= 0)
         {
             printf("\nDetalles del Prestamo:\n");
-            printf("\tId: %d\n", aux.id);
-            printf("\tUsuario:  %s\n", aux.usuario);
-            printf("\tEstado: %d\n", aux.estado);
-            printf("\tNombre:  %s\n", aux.nombre);
-            printf("\tFecha de inicio:  %s\n", aux.fechaInicio);
-            printf("\tFecha de fin:  %s\n\n", aux.fechaFin);
+            printf("\tId: %d\n", aux->id);
+            printf("\tUsuario:  %s\n", aux->usuario);
+            printf("\tEstado: %d\n", aux->estado);
+            printf("\tNombre:  %s\n", aux->nombre);
+            printf("\tFecha de inicio:  %s\n", aux->fechaInicio);
+            printf("\tFecha de fin:  %s\n\n", aux->fechaFin);
         }
     }
 
@@ -49,22 +49,22 @@ void incluir_Prestamo(listaPrestamos* l)
 {
     Prestamo* prestamo = calloc(1, sizeof(Prestamo));
     
-    prestamo.id = l->tam + 1;
+    prestamo->id = l->tam + 1;
     printf("Ingrese el nombre del usuario: ");
-    scanf(" %[^\n]s", &prestamo.usuario);
+    scanf(" %[^\n]s", prestamo->usuario);
     printf("Ingrese el estado del prestamo: ");
-    scanf("%d", &prestamo.estado);
+    scanf("%d", prestamo->estado);
     printf("Ingrese el nombre del libro: ");
-    scanf(" %[^\n]s", &prestamo.nombre);
+    scanf(" %[^\n]s", prestamo->nombre);
     while(true){
         printf("Ingrese la fecha de inicio del prestamo: ");
-        scanf(" %[^\n]s", &prestamo.fechaInicio);
+        scanf(" %[^\n]s", prestamo->fechaInicio);
         
         printf("Ingrese la fecha de fin del prestamo: ");
-        scanf(" %[^\n]s", &prestamo.fechaFin);
+        scanf(" %[^\n]s", prestamo->fechaFin);
         
-        if (esFechaValida(prestamo.fechaInicio) && esFechaValida(prestamo.fechaFin)){
-            if(compararFechas(prestamo.fechaInicio, prestamo.fechaFin) == 1){
+        if (esFechaValida(prestamo->fechaInicio) && esFechaValida(prestamo->fechaFin)){
+            if(compararFechas(prestamo->fechaInicio, prestamo->fechaFin) == 1){
                 break;
             }
             else{
@@ -82,19 +82,19 @@ void incluir_Usuario(listaUsuarios* l)
 {
     Usuario* usuario = calloc(1, sizeof(Usuario));
     printf("Ingrese la identificacion del usuario: ");
-    scanf("%d", &usuario.id);
+    scanf("%d", &usuario->id);
 
     printf("Ingrese el nombre del usuario: ");
-    scanf("  %[^\n]s", &usuario.nombre);
+    scanf("  %[^\n]s", usuario->nombre);
     printf("Ingrese la direccion del usuario: ");
-    scanf("  %[^\n]s", &usuario.direccion);
+    scanf("  %[^\n]s", usuario->direccion);
     
     addUsuario(l, usuario);
 }
 
 void recuperarLibrosTxt(listaLibros* l) 
 {
-    FILE *archivo = fopen("doc.txt", "r");
+    FILE *archivo = fopen("doc->txt", "r");
     char linea[200];
     
     while (fgets(linea, sizeof(linea), archivo)) {
@@ -102,29 +102,29 @@ void recuperarLibrosTxt(listaLibros* l)
         Libro* libro = calloc(1, sizeof(Libro));
         
         char *palabra = strtok(linea, "|");
-        strcpy(libro.nombre, palabra);
+        strcpy(libro->nombre, palabra);
         
         palabra = strtok(NULL, "|");
-        strcpy(libro.autor, palabra);
+        strcpy(libro->autor, palabra);
         
         palabra = strtok(NULL, "|");
-        libro.anio = strtol(palabra, NULL, 10);
+        libro->anio = strtol(palabra, NULL, 10);
         
         palabra = strtok(NULL, "|");
-        strcpy(libro.genero, palabra);
+        strcpy(libro->genero, palabra);
         
         palabra = strtok(NULL, "|");
-        strcpy(libro.resumen, palabra);
+        strcpy(libro->resumen, palabra);
         
         palabra = strtok(NULL, "|");
-        libro.cantidad = strtol(palabra, NULL, 10);
+        libro->cantidad = strtol(palabra, NULL, 10);
         
-        printf("Título:  %s\n", libro.nombre);
-        printf("Autor:  %s\n", libro.autor);
-        printf("Año: %d\n", libro.anio);
-        printf("Género:  %s\n", libro.genero);
-        printf("Descripción:  %s\n", libro.resumen);
-        printf("Disponibles: %d\n", libro.cantidad);
+        printf("Título:  %s\n", libro->nombre);
+        printf("Autor:  %s\n", libro->autor);
+        printf("Año: %d\n", libro->anio);
+        printf("Género:  %s\n", libro->genero);
+        printf("Descripción:  %s\n", libro->resumen);
+        printf("Disponibles: %d\n", libro->cantidad);
         addLibro(l, libro);
         
         printf("\n");
